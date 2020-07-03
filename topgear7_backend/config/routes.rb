c2fc0f80a5598c2bd935f55b30fee34dd97c61f6
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-03 15:23:12 -0400

    config routes with an 'api/v1' scope

--- topgear7_backend/config/routes.rb
+++ topgear7_backend/config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
   scope '/api/v1' do
-    resources :users, only: :index
+    # resources :users, only: :index
     resources :routes, only: [:index, :show], param: :bus_line_name
     resources :buses, only: [:index, :update, :show], param: :route_name
     resources :activities, only: [:index, :show, :create, :update], param: :vehicle
-
-  # get 'activities/index'
-  # get 'activities/create'
-  # get 'activities/show'
-  # get 'activities/update'
-  # get 'buses/index'
-  # get 'buses/show'
-  # get 'buses/update'
-  # get 'routes/index'
-  # get 'routes/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

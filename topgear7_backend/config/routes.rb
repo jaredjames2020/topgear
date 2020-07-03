3a558ab4ae606c6e096db848b603f3b5610788da
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-03 15:24:16 -0400

    fixed syntax error

--- topgear7_backend/config/routes.rb
+++ topgear7_backend/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
     resources :buses, only: [:index, :update, :show], param: :route_name
     resources :activities, only: [:index, :show, :create, :update], param: :vehicle
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end

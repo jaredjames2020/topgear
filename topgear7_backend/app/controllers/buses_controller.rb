8f02bd6058e0645d410b25b912a077fa48ac6d38
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-03 15:19:00 -0400

    update Buses controller

--- topgear7_backend/app/controllers/buses_controller.rb
+++ topgear7_backend/app/controllers/buses_controller.rb
@@ -1,10 +1,17 @@
 class BusesController < ApplicationController
+
   def index
+    render :json => Bus.all, :include => :route
   end
 
   def show
+    render :json => Bus.find_by(route_name: params[:route_name])
   end
 
   def update
+    bus = Bus.find_by(route_name: params[:route_name])
+    bus.update(congestion: params[:congestion])
+    render :json => bus
   end
+
 end

e52deaf2c500ff3623aea84429201fd6a2272ee3
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-07 12:32:59 -0400

    filter API bus data render search and dropdown

--- topgear7_frontend/src/components/Routelist.js
+++ topgear7_frontend/src/components/Routelist.js
@@ -2,7 +2,9 @@ import React, {Component} from "react";
 
 class Routelist extends Component {
   state = {
-    route_data: []
+    route_data: [],
+    input_route_search: "",
+    selected_route: ""
   };
 
   async componentDidMount() {
@@ -11,10 +13,42 @@ class Routelist extends Component {
     this.setState({route_data: data});
   }
 
+  inputHandleOnChange = event => {
+    this.setState({input_route_search: event.target.value});
+  };
+
+  selectedHandleOnChange = event => {
+    this.setState({selected_route: event.target.value});
+  };
+
   render() {
+    const {input_route_search} = this.state;
+    const filteredRoutes = this.state.route_data.filter(route => {
+      if (input_route_search !== "") {
+        return (
+          route.route_name
+            .toLowerCase()
+            .indexOf(input_route_search.toLowerCase()) !== -1
+        );
+      }
+    });
+
     return (
       <div>
-        <h1> HOME </h1>
+        <input
+          name="text"
+          type="text"
+          placeholder="Search Bus Routes"
+          onChange={this.inputHandleOnChange}
+        />
+        <input type="submit" />
+        <select id="route_name" onChange={this.selectedHandleOnChange}>
+          {filteredRoutes.reverse().map((name, i) => (
+            <option value={name.route_name} key={i}>
+              {name.route_name}
+            </option>
+          ))}
+        </select>
       </div>
     );
   }

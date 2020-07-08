f2859fcf23903d07ae3d05295563d3724f4a5891
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-07 20:16:31 -0400

    placed MTA data fetch on main App render

--- topgear7_frontend/src/App.js
+++ topgear7_frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Routelist from "./components/Routelist";
 import Routestatus from "./components/Routestatus";
@@ -7,11 +6,26 @@ import Routestatus from "./components/Routestatus";
 console.log(process.env.REACT_APP_MTA_API_KEY);
 
 class App extends Component {
+  state = {
+    mta_vehicle_activity: []
+  };
+
+  async componentDidMount() {
+    await fetch("http://localhost:5000/vehicle")
+      .then(resp => resp.json())
+      .then(data => {
+        const bus_activity =
+          data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0]
+            .VehicleActivity;
+        this.setState({mta_vehicle_activity: bus_activity});
+      });
+  }
+
   render() {
     return (
       <div>
         <Routelist />
-        <Routestatus />
+        <Routestatus activity={this.state.mta_vehicle_activity} />
       </div>
     );
   }

18b44b6fdad6deae36a7480bbe5d6f16b4803fca
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-11 19:11:24 -0400

    pass vehicle activity to Routefinder component

--- topgear7_frontend/src/containers/MTAContainer.js
+++ topgear7_frontend/src/containers/MTAContainer.js
@@ -1,9 +1,25 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import Routefinder from "../components/Routefinder";
 
 class MTAContainer extends Component {
   render() {
-    return <div>HELLO WORLD</div>;
+    return (
+      <div>
+        <Routefinder vehicleActivity={this.props.VehicleActivity} />
+      </div>
+    );
   }
 }
 
-export default MTAContainer;
+const mapStateToProps = state => {
+  return {
+    VehicleActivity: state.bus_data
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  findRoute: route => dispatch({type: "FIND_ROUTE", route})
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MTAContainer);

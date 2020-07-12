bd43da1ba4c2386de95642668221d7d7370c1e05
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-11 20:44:21 -0400

    find routes with input text, list routes in select

--- topgear7_frontend/src/components/Routefinder.js
+++ topgear7_frontend/src/components/Routefinder.js
@@ -1,42 +1,64 @@
 import React, {Component} from "react";
 
-class Routelist extends Component {
-  // state = {
-  //   bus_data: [],
-  //   input_route_search: "",
-  //   selected_route: ""
-  // };
-  //
-  // inputHandleOnChange = event => {
-  //   this.setState({input_route_search: event.target.value});
-  // };
-  //
-  // selectedHandleOnChange = event => {
-  //   this.setState({selected_route: event.target.value});
-  // };
-  //
-  // displayName = () => {
-  //   const findName = this.route_data.find(
-  //     name => name.route_name === this.state.selected_route
-  //   );
-  //   console.log(findName);
-  //   return findName.route_destination;
-  // };
+class Routefinder extends Component {
+  state = {
+    input_route_search: "",
+    selected_route: ""
+  };
+
+  inputHandleOnChange = event => {
+    this.setState({input_route_search: event.target.value});
+  };
+
+  selectedHandleOnChange = event => {
+    this.setState({selected_route: event.target.value});
+  };
 
   render() {
-    console.log(this.props.vehicleActivity);
-    // const {input_route_search} = this.state;
-    // const filteredRoutes = this.state.route_data.filter(route => {
-    //   if (input_route_search !== "") {
-    //     return (
-    //       route.route_name
-    //         .toLowerCase()
-    //         .indexOf(input_route_search.toLowerCase()) !== -1
-    //     );
-    //   }
-    // });
+    const {input_route_search} = this.state;
 
-    return <div>HOME</div>;
+    const inputFilteredRoutes = this.props.vehicleActivity.filter(route => {
+      if (input_route_search !== "") {
+        return (
+          route.MonitoredVehicleJourney.PublishedLineName.toLowerCase().indexOf(
+            input_route_search.toLowerCase()
+          ) !== -1
+        );
+      }
+    });
+
+    const uniqueRoutes = inputFilteredRoutes
+      .reverse()
+      .map(name => name.MonitoredVehicleJourney.PublishedLineName);
+
+    let unique = [...new Set(uniqueRoutes)].sort();
+
+    console.log(this.props.vehicleActivity);
+    console.log(inputFilteredRoutes);
+    console.log(uniqueRoutes);
+    console.log(unique.sort());
+
+    return (
+      <div>
+        <input
+          name="text"
+          type="text"
+          placeholder="Search Bus Routes"
+          onChange={this.inputHandleOnChange}
+        />
+        <input type="submit" />
+        <select id="route_name" onChange={this.selectedHandleOnChange}>
+          {unique.map((name, i) => (
+            <option value={name} key={i}>
+              {name}
+            </option>
+          ))}
+        </select>
+
+        <h4>{this.findName}</h4>
+      </div>
+    );
   }
 }
-export default Routelist;
+
+export default Routefinder;

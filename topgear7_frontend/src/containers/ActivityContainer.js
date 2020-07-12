5c52effbc708e699c470000ca7b48af67405ba1a
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-12 19:31:39 -0400

    syntax update

--- topgear7_frontend/src/containers/ActivityContainer.js
+++ topgear7_frontend/src/containers/ActivityContainer.js
@@ -1,13 +1,19 @@
 import React, {Component} from "react";
 // import {connect} from "react-redux";
 import Routeactivitytable from "../components/Routeactivitytable";
+import Routelist from "../components/Routelist";
 
 class ActivityContainer extends Component {
   render() {
     return (
       <div>
         <Routeactivitytable
-          currentActivity={this.props.selected_route}
+          selected_route={this.props.selected_route}
+          directionRef0={this.props.directionRef0}
+          directionRef1={this.props.directionRef1}
+        />
+        <Routelist
+          selected_route={this.props.selected_route}
           directionRef0={this.props.directionRef0}
           directionRef1={this.props.directionRef1}
         />

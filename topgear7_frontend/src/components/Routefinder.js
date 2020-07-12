d0b4a4a93da3c80e3d6e3bd669cbbad1604c8d71
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-12 19:32:00 -0400

    syntax update

--- topgear7_frontend/src/components/Routefinder.js
+++ topgear7_frontend/src/components/Routefinder.js
@@ -4,7 +4,9 @@ import ActivityContainer from "../containers/ActivityContainer";
 class Routefinder extends Component {
   state = {
     input_route_search: "",
-    selected_route: ""
+    selected_route: "",
+    directionRef0: [],
+    directionRef1: []
   };
 
   inputHandleOnChange = event => {
@@ -34,11 +36,27 @@ class Routefinder extends Component {
 
     let unique = [...new Set(uniqueRoutes)].sort();
 
+    const directionRef0 = inputFilteredRoutes.filter(
+      route =>
+        route.MonitoredVehicleJourney.PublishedLineName.toLowerCase() ===
+          this.state.selected_route.toLowerCase() &&
+        route.MonitoredVehicleJourney.DirectionRef === "0"
+    );
+
+    const directionRef1 = inputFilteredRoutes.filter(
+      route =>
+        route.MonitoredVehicleJourney.PublishedLineName.toLowerCase() ===
+          this.state.selected_route.toLowerCase() &&
+        route.MonitoredVehicleJourney.DirectionRef === "1"
+    );
+
     console.log(this.props.vehicleActivity);
     console.log(inputFilteredRoutes);
     console.log(uniqueRoutes);
     console.log(unique.sort());
     console.log(this.state.selected_route);
+    console.log(directionRef0);
+    console.log(directionRef1);
 
     return (
       <div>
@@ -48,19 +66,23 @@ class Routefinder extends Component {
           placeholder="Search Bus Routes"
           onChange={this.inputHandleOnChange}
         />
-        <input type="submit" />
         <select id="route_name" onChange={this.selectedHandleOnChange}>
+          {" "}
           {unique.map((name, i) => (
             <option value={name} key={i}>
               {name}
             </option>
           ))}
         </select>
-
-        <ActivityContainer selected_route={this.state.selected_route} />
+        <ActivityContainer
+          selected_route={this.state.selected_route}
+          directionRef0={directionRef0}
+          directionRef1={directionRef1}
+        />
       </div>
     );
   }
 }
-
+//refactor dropdown to a selectable table
+//remove submit from input "type=submit"
 export default Routefinder;

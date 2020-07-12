e84c306dc24c7ed47a8c6eb27d43274ba5de6247
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-12 19:31:23 -0400

    render route list for selected route

--- topgear7_frontend/src/components/Routelist.js
+++ topgear7_frontend/src/components/Routelist.js
@@ -2,53 +2,77 @@ import React, {Component} from "react";
 
 class Routelist extends Component {
   state = {
-    route_data: [],
-    input_route_search: "",
-    selected_route: ""
+    route_data: []
   };
 
   async componentDidMount() {
-    const res = await fetch("http://localhost:3001/api/buses");
+    const res = await fetch("http://localhost:3001/api/routes");
     const data = await res.json();
     this.setState({route_data: data});
   }
 
-  inputHandleOnChange = event => {
-    this.setState({input_route_search: event.target.value});
-  };
+  renderTableName(direction) {
+    return direction.map((bus, index) => {
+      let desName = bus.MonitoredVehicleJourney.DestinationName;
+      return desName;
+    });
+  }
 
-  selectedHandleOnChange = event => {
-    this.setState({selected_route: event.target.value});
+  renderName(array) {
+    return array[0];
+  }
+
+  renderRouteInfo() {
+    return this.state.route_data.filter(
+      route => route.bus_line_name === this.props.selected_route
+    );
+  }
+
+  renderStopList() {
+    let yup = [];
+    this.renderRouteInfo().forEach(stop => yup.push(stop.service_stops));
+    return yup;
+  }
+
+  renderStopName(array) {
+    let go = [];
+    let stops = array;
+    let run = stops.toString().split(", ");
+    for (var i = 0; i < run.length; i++) {
+      go.push(
+        <tr key={i}>
+          <td key={[i]}>{run[i]}</td>
+        </tr>
+      );
+    }
+    return go;
+  }
+
+  handleOnClick = event => {
+    console.log("LINK TO ROUTES");
   };
 
   render() {
-    const {input_route_search} = this.state;
-    const filteredRoutes = this.state.route_data.filter(route => {
-      if (input_route_search !== "") {
-        return (
-          route.route_name
-            .toLowerCase()
-            .indexOf(input_route_search.toLowerCase()) !== -1
-        );
-      }
-    });
+    console.log(this.state.route_data);
+    console.log(this.renderStopName(this.renderStopList()));
 
     return (
       <div>
-        <input
-          name="text"
-          type="text"
-          placeholder="Search Bus Routes"
-          onChange={this.inputHandleOnChange}
-        />
-        <input type="submit" />
-        <select id="route_name" onChange={this.selectedHandleOnChange}>
-          {filteredRoutes.reverse().map((name, i) => (
-            <option value={name.route_name} key={i}>
-              {name.route_name}
-            </option>
-          ))}
-        </select>
+        <h3 id="title">Route: {this.props.selected_route}</h3>
+        <h4>
+          To: {this.renderName(this.renderTableName(this.props.directionRef0))}
+        </h4>
+        <table id="dir0_activity_table">
+          <tbody>{this.renderStopName(this.renderStopList())}</tbody>
+        </table>
+        <h4>
+          To: {this.renderName(this.renderTableName(this.props.directionRef1))}
+        </h4>
+        <table id="dir1_activity_table">
+          <tbody>{this.renderStopName(this.renderStopList()).reverse()}</tbody>
+        </table>
+
+        <button onClick={this.handleOnClick}>Search more routes</button>
       </div>
     );
   }

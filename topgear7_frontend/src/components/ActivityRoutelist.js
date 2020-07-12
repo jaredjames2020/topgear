8dee40a0bd02bf5bff72836eb174e84a1fb73473
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-12 08:22:26 -0400

    add ActivityRoutelist component

---
+++ topgear7_frontend/src/components/ActivityRoutelist.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class ActivityRoutelist extends Component {
+  render() {
+    console.log(this.props.currentActivity);
+    return <div>ACTIVITY</div>;
+  }
+}
+
+export default ActivityRoutelist;

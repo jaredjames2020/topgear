284fe61d3c7d5b33f52c0aaf6f00121a34140e6e
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-15 20:41:00 -0400

    create Situationstable component

---
+++ topgear7_frontend/src/components/Situationtable.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Situationtable extends Component {
+  render() {
+    console.log(this.props.situations);
+    return (
+      <div>
+        <h1>SITUATION</h1>
+      </div>
+    );
+  }
+}
+
+export default Situationtable;

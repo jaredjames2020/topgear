95c762b92fb94edf02e805867afc3a1696c7752e
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-15 20:40:24 -0400

    add Situations Actions - fetch

---
+++ topgear7_frontend/src/actions/situationActions.js
@@ -0,0 +1,17 @@
+export const fetchSituations = () => {
+  const API_ENDPOINT = "http://localhost:5000/vehicle";
+  return dispatch => {
+    dispatch({type: "LOAD_SITUATIONS"});
+    fetch(API_ENDPOINT)
+      .then(response => response.json())
+      .then(data => {
+        dispatch({
+          type: "ADD_SITUATIONS",
+          situations:
+            data.Siri.ServiceDelivery.SituationExchangeDelivery[0].Situations
+        });
+      })
+
+      .catch(error => console.error(error));
+  };
+};

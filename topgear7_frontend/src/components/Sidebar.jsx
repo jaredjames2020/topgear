a17aee76afe8d0c1ca5541fcbaf2ebb2777c26cd
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-17 17:14:28 -0400

    add Sidebar component

---
+++ topgear7_frontend/src/components/Sidebar.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <div class="d-flex" id="wrapper">
+          <div className="bg-light border-right" id="sidebar-wrapper">
+            <div className="sidebar-heading">
+              TOP GEAR: <br></br>Where do you want to go?
+            </div>
+            <div className="list-group list-group-flush">
+              <Link
+                to="/"
+                className="list-group-item list-group-item-action bg-light"
+              >
+                HOME
+              </Link>
+              <Link
+                to="/busroute"
+                className="list-group-item list-group-item-action bg-light"
+              >
+                ROUTES
+              </Link>
+              <Link
+                to="/situation"
+                className="list-group-item list-group-item-action bg-light"
+              >
+                SITUATION STATUS
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;

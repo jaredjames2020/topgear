1ec4e98a276c9393721a3aa7deef53b44a41cb44
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-18 16:07:02 -0400

    update state prop

--- topgear7_frontend/src/components/Routelist.js
+++ topgear7_frontend/src/components/Routelist.js
@@ -1,14 +1,14 @@
 import React, {Component} from "react";
 
 class Routelist extends Component {
-  // state = {
-  //   route_data: []
-  // };
-
   constructor() {
     super();
     this.state = {
-      route_data: []
+      route_data: [],
+      input_route_search: "",
+      selected_route: "",
+      directionRef0: [],
+      directionRef1: []
     };
   }
 
@@ -18,21 +18,32 @@ class Routelist extends Component {
     this.setState({route_data: data});
   }
 
-  renderTableName(direction) {
-    return direction.map((bus, index) => {
-      let desName = bus.MonitoredVehicleJourney.DestinationName;
+  inputHandleOnChange = event => {
+    this.setState({input_route_search: event.target.value});
+  };
+
+  selectedHandleOnChange = event => {
+    this.setState({selected_route: event.target.value});
+  };
+
+  renderRouteInfo() {
+    return this.state.route_data.filter(
+      route => route.bus_line_name === this.state.selected_route
+    );
+  }
+
+  renderTableNameDes() {
+    return this.renderRouteInfo().map((bus, index) => {
+      let desName = bus.service_destination;
       return desName;
     });
   }
 
-  renderName(array) {
-    return array[0];
-  }
-
-  renderRouteInfo() {
-    return this.state.route_data.filter(
-      route => route.bus_line_name === this.props.selected_route
-    );
+  renderTableNameOri() {
+    return this.renderRouteInfo().map((bus, index) => {
+      let oriName = bus.service_origin;
+      return oriName;
+    });
   }
 
   renderStopList() {
@@ -45,6 +56,7 @@ class Routelist extends Component {
     let go = [];
     let stops = array;
     let run = stops.toString().split(", ");
+    console.log(run);
     for (var i = 0; i < run.length; i++) {
       go.push(
         <tr key={i}>
@@ -60,28 +72,66 @@ class Routelist extends Component {
   };
 
   render() {
-    console.log(this.state.route_data);
-    console.log(this.renderStopName(this.renderStopList()));
+    const {input_route_search} = this.state;
+
+    const inputFilteredRoutes = this.state.route_data.filter(route => {
+      if (input_route_search !== "") {
+        return (
+          route.bus_line_name
+            .toLowerCase()
+            .indexOf(input_route_search.toLowerCase()) !== -1
+        );
+      }
+    });
+
+    const uniqueRoutes = inputFilteredRoutes
+      .reverse()
+      .map(name => name.bus_line_name);
+
+    let unique = [...new Set(uniqueRoutes)].sort();
 
     return (
       <div>
-        <h3 id="title">Route: {this.props.selected_route}</h3>
-        <h4>
-          To: {this.renderName(this.renderTableName(this.props.directionRef0))}
-        </h4>
+        <input
+          name="text"
+          type="text"
+          placeholder="Search Bus Routes"
+          onChange={this.inputHandleOnChange}
+        />
+        <select id="route_name" onChange={this.selectedHandleOnChange}>
+          {" "}
+          {unique.map((name, i) => (
+            <option value={name} key={i}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <p id="title">Route: {this.props.selected_route}</p>
+        <p>To: {this.renderTableNameDes()}</p>
         <table id="dir0_activity_table">
           <tbody>{this.renderStopName(this.renderStopList())}</tbody>
         </table>
-        <h4>
-          To: {this.renderName(this.renderTableName(this.props.directionRef1))}
-        </h4>
+        <br></br>
+        <p>To: {this.renderTableNameOri()}</p>
         <table id="dir1_activity_table">
           <tbody>{this.renderStopName(this.renderStopList()).reverse()}</tbody>
         </table>
-
-        <button onClick={this.handleOnClick}>Search more routes</button>
       </div>
     );
   }
 }
 export default Routelist;
+// <p id="title">Route: {this.props.selected_route}</p>
+// <p>
+//   To: {this.renderName(this.renderTableName(this.props.directionRef0))}
+// </p>
+// <table id="dir0_activity_table">
+//   <tbody>{this.renderStopName(this.renderStopList())}</tbody>
+// </table>
+// <p>
+//   To: {this.renderName(this.renderTableName(this.props.directionRef1))}
+// </p>
+// <table id="dir1_activity_table">
+//   <tbody>{this.renderStopName(this.renderStopList()).reverse()}</tbody>
+// </table>
+// <button onClick={this.handleOnClick}>Search more routes</button>

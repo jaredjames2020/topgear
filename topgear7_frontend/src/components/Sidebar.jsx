71bd0c36f4eaceddcfd5733487e0051acddf39ec
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-20 06:01:14 -0400

    bootstrap styling

--- topgear7_frontend/src/components/Sidebar.jsx
+++ topgear7_frontend/src/components/Sidebar.jsx
@@ -1,15 +1,17 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+// import {Container, Row, Col} from "react-bootstrap";
 
 class Sidebar extends Component {
   render() {
     return (
       <div>
         <div className="d-flex" id="wrapper">
-          <div className="bg-light border-right" id="sidebar-wrapper">
-            <div className="sidebar-heading">
-              TOP GEAR: <br></br>Where do you want to go?
-            </div>
+          <div
+            className="bg-dark border-right text-white text-center rounded-lg"
+            id="sidebar-wrapper"
+          >
+            <div className="sidebar-heading">TOP GEAR</div>
             <div className="list-group list-group-flush">
               <Link
                 to="/"

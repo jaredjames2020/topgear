f445fc2e9cbb638f3d1f3f603a2d228d86d9c882
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-20 06:03:30 -0400

    refactor

--- topgear7_frontend/src/containers/RouteContainer.js
+++ topgear7_frontend/src/containers/RouteContainer.js
@@ -1,19 +1,20 @@
 import React, {Component} from "react";
-import Routelist from "../components/Routelist";
+import Routelist from "../components/Route/Routelist";
 
 class RouteContainer extends Component {
   render() {
     return (
       <div>
         ROUTES
-        <Routelist
-          selected_route={this.props.selected_route}
-          directionRef0={this.props.directionRef0}
-          directionRef1={this.props.directionRef1}
-        />
+        <Routelist />
       </div>
     );
   }
 }
 
 export default RouteContainer;
+// <Routelist
+//   selected_route={this.props.selected_route}
+//   directionRef0={this.props.directionRef0}
+//   directionRef1={this.props.directionRef1}
+// />

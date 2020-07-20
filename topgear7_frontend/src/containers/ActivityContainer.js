c2593d47342017be52111cca08f5bc9f46d683c3
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-20 06:03:10 -0400

    refactor

--- topgear7_frontend/src/containers/ActivityContainer.js
+++ topgear7_frontend/src/containers/ActivityContainer.js
@@ -1,37 +1,37 @@
-import React, {Component} from "react";
-// import {connect} from "react-redux";
-import Routeactivitytable from "../components/Routeactivitytable";
-import Routelist from "../components/Routelist";
-
-class ActivityContainer extends Component {
-  render() {
-    return <div>ACTIVITY</div>;
-  }
-}
-
-// <Routeactivitytable
-//   selected_route={this.props.selected_route}
-//   directionRef0={this.props.directionRef0}
-//   directionRef1={this.props.directionRef1}
-// />
-// <Routelist
-//   selected_route={this.props.selected_route}
-//   directionRef0={this.props.directionRef0}
-//   directionRef1={this.props.directionRef1}
-// />
-
-// const mapStateToProps = state => {
-//   return {
-//     currentActivity: state.currentActivity
-//   };
-// };
+// import React, {Component} from "react";
+// // import {connect} from "react-redux";
+// import Routeactivitytable from "../components/Routeactivitytable";
+// import Routelist from "../components/Routelist";
 //
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     currentActivity: addActivity =>
-//       dispatch({type: "ADD_ACTIVITY", addActivity})
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(ActivityContainer);
-export default ActivityContainer;
+// class ActivityContainer extends Component {
+//   render() {
+//     return <div>ACTIVITY</div>;
+//   }
+// }
+//
+// // <Routeactivitytable
+// //   selected_route={this.props.selected_route}
+// //   directionRef0={this.props.directionRef0}
+// //   directionRef1={this.props.directionRef1}
+// // />
+// // <Routelist
+// //   selected_route={this.props.selected_route}
+// //   directionRef0={this.props.directionRef0}
+// //   directionRef1={this.props.directionRef1}
+// // />
+//
+// // const mapStateToProps = state => {
+// //   return {
+// //     currentActivity: state.currentActivity
+// //   };
+// // };
+// //
+// // const mapDispatchToProps = dispatch => {
+// //   return {
+// //     currentActivity: addActivity =>
+// //       dispatch({type: "ADD_ACTIVITY", addActivity})
+// //   };
+// // };
+//
+// // export default connect(mapStateToProps, mapDispatchToProps)(ActivityContainer);
+// export default ActivityContainer;

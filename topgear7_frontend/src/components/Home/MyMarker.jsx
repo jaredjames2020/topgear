c8530b655631fe0026d3a19c56d0dd11116a8ad2
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-20 05:59:06 -0400

    refactor

---
+++ topgear7_frontend/src/components/Home/MyMarker.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Marker.css";
+
+const MyMarker = (props: any) => {
+  const {color, name} = props;
+  return (
+    <div>
+      <div
+        className="pin bounce"
+        style={{backgroundColor: color, cursor: "pointer"}}
+        title={name}
+      />
+      <div className="pulse" />
+    </div>
+  );
+};
+
+export default MyMarker;

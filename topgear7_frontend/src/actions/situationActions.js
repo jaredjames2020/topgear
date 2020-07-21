a691c5aa2ca632ee0105044528cf8006040a9a2c
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-21 11:50:45 -0400

    refactor axios

--- topgear7_frontend/src/actions/situationActions.js
+++ topgear7_frontend/src/actions/situationActions.js
@@ -1,18 +1,18 @@
+import axios from "axios";
+
 export const fetchSituations = () => {
   const API_ENDPOINT = "http://localhost:5000/vehicle";
-  return dispatch => {
+  return async dispatch => {
     dispatch({type: "LOAD_SITUATIONS"});
-    fetch(API_ENDPOINT)
-      .then(response => response.json())
-      .then(data => {
-        dispatch({
-          type: "ADD_SITUATIONS",
-          situations:
-            data.Siri.ServiceDelivery.SituationExchangeDelivery[0].Situations,
-          loading: true
-        });
-      })
-
-      .catch(error => console.error(error));
+    const response = await axios.get(API_ENDPOINT);
+    const data = response.data;
+    const situations =
+      data.Siri.ServiceDelivery.SituationExchangeDelivery[0].Situations;
+    const loading = true;
+    dispatch({
+      type: "ADD_SITUATIONS",
+      situations,
+      loading
+    });
   };
 };

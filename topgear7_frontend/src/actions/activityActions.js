d4e71c274ed3de1c5993957d2ad05a699dbfa97e
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-21 00:44:21 -0400

    refactor

--- topgear7_frontend/src/actions/activityActions.js
+++ topgear7_frontend/src/actions/activityActions.js
@@ -1,7 +1,37 @@
-console.log("HITTING");
-export const addActivity = () => {
-  return {
-    type: "ADD_ACTIVITY",
-    currentActivity: ["1", "2", "3"]
+import axios from "axios";
+
+export function addCongestionActivity(activity) {
+  return async dispatch => {
+    const response = await axios.post("http://localhost:3001/api/activities", {
+      vehicle: activity[0],
+      congestion: activity[1]
+    });
+    const activity_data = response.data;
+    dispatch({type: "ADD_CONGESTION_ACTIVITY", activity_data});
   };
-};
+}
+
+export function addDelayActivity(activity) {
+  return async dispatch => {
+    const response = await axios.post("http://localhost:3001/api/activities", {
+      vehicle: activity[0],
+      delay: activity[1]
+    });
+    const activity_data = response.data;
+    dispatch({type: "ADD_DELAY_ACTIVITY", activity_data});
+  };
+}
+
+// export function addActivity(["MTA NYCT_4019", "limitedseats"]) {
+//   return async dispatch => {
+//     const response = await axios.post("http://localhost:3001/api/activities", {
+//       congestion: activity[1],
+//       delay: "OUt",
+//       vehicle: activity[0],
+//       route_name: "Queens",
+//       route_destination: "Home"
+//     });
+//     const activity_data = response.data;
+//     dispatch({type: "ADD_ACTIVITY", activity_data});
+//   };
+// }

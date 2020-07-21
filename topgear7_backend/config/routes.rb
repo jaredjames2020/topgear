eba679ad052a4b3a5d367f13f7b03e16a2c44844
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-21 00:45:13 -0400

    refactor

--- topgear7_backend/config/routes.rb
+++ topgear7_backend/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     # resources :users, only: :index
     resources :routes, only: [:index, :show], param: :bus_line_name
     resources :buses, only: [:index, :update, :show], param: :route_name
-    resources :activities, only: [:index, :show, :create, :update], param: :vehicle
+    resources :activities
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
+# resources :activities, only: [:index, :show, :create, :update], param: :vehicle

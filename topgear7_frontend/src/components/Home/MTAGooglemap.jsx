209f9c1e780bf588e7dcb45544b7d4faef3f6be7
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-21 23:22:10 -0400

    refactor map to forEach

--- topgear7_frontend/src/components/Home/MTAGooglemap.jsx
+++ topgear7_frontend/src/components/Home/MTAGooglemap.jsx
@@ -7,7 +7,7 @@ const KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 const MTAGooglemap = (props: any) => {
   const renderLatLong = (direction, color) => {
     let array = [];
-    direction.map((bus, index) => {
+    direction.forEach((bus, index) => {
       let lati = bus.MonitoredVehicleJourney.VehicleLocation.Latitude;
       let long = bus.MonitoredVehicleJourney.VehicleLocation.Longitude;
       array.push({lat: lati, lng: long, index: index});
@@ -20,7 +20,7 @@ const MTAGooglemap = (props: any) => {
 
   const renderCenter = () => {
     let array = [];
-    props.directionRef0.map((bus, index) => {
+    props.directionRef0.forEach((bus, index) => {
       let lati = bus.MonitoredVehicleJourney.VehicleLocation.Latitude;
       let long = bus.MonitoredVehicleJourney.VehicleLocation.Longitude;
       array.push({lat: lati, lng: long});
@@ -29,19 +29,21 @@ const MTAGooglemap = (props: any) => {
   };
 
   return (
-    <div style={{height: "100vh", width: "100%"}}>
-      <GoogleMapReact
-        bootstrapURLKeys={{key: KEY}}
-        center={
-          renderCenter()
-            ? renderCenter()
-            : {lat: 40.69879315, lng: -73.88767071}
-        }
-        defaultZoom={12}
-      >
-        {renderLatLong(props.directionRef0, "blue")}
-        {renderLatLong(props.directionRef1, "red")}
-      </GoogleMapReact>
+    <div className="map">
+      <div style={{height: "100vh", width: "100%"}}>
+        <GoogleMapReact
+          bootstrapURLKeys={{key: KEY}}
+          center={
+            renderCenter()
+              ? renderCenter()
+              : {lat: 40.69879315, lng: -73.88767071}
+          }
+          defaultZoom={12}
+        >
+          {renderLatLong(props.directionRef0, "blue")}
+          {renderLatLong(props.directionRef1, "red")}
+        </GoogleMapReact>
+      </div>
     </div>
   );
 };

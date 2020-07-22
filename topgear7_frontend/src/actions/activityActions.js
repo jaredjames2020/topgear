08405a4aed4e8cf799ce71987752943ef05a2205
jaredjames2020 <jaredjames2020@gmail.com>
2020-07-21 23:21:42 -0400

    refactor

--- topgear7_frontend/src/actions/activityActions.js
+++ topgear7_frontend/src/actions/activityActions.js
@@ -21,17 +21,3 @@ export function addDelayActivity(activity) {
     dispatch({type: "ADD_DELAY_ACTIVITY", activity_data});
   };
 }
-
-// export function addActivity(["MTA NYCT_4019", "limitedseats"]) {
-//   return async dispatch => {
-//     const response = await axios.post("http://localhost:3001/api/activities", {
-//       congestion: activity[1],
-//       delay: "OUt",
-//       vehicle: activity[0],
-//       route_name: "Queens",
-//       route_destination: "Home"
-//     });
-//     const activity_data = response.data;
-//     dispatch({type: "ADD_ACTIVITY", activity_data});
-//   };
-// }
